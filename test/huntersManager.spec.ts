d50c13248b4e9c2f17ad0c0fad3005aa6b9be3d6
Iker Díaz Cabrera <alu0101545455@ull.edu.es>
2025-05-06 11:17:55 +0000

    Example test added

--- test/huntersManager.spec.ts
+++
@@ -1,4 +0,0 @@
-import { describe, test, expect, beforeEach, vi, afterEach } from 'vitest';
-import { CustomerManager } from '../src/huntersManager.js';
-import { Hunter, HunterDocumentInterface } from '../src/hunters.js';
-
